4c30a7925f1c83a23b377c0643beee18a37e08de
M.Mahdavi <mahdavi@sahab.ir>
2024-07-13 17:24:57 +0330

    SImage update

--- src/components/SImage.vue
+++ src/components/SImage.vue
@@ -1,52 +1,50 @@
 <script setup>
-import no_image from '@/assets/img/no-image-icon.png'
-import axios from "axios"
-const url = import.meta.env.VITE_FILE_URL
-const props = defineProps(['src', 'path', 'class', 'alt', 'style'])
-const emits = defineEmits(["update:modelValue","click"]) 
-const image = ref(no_image)
+import no_image from '@/assets/img/no-image-icon.png';
+import axios from 'axios';
 
-onMounted(() => {
-  if (props.src && props.src != ''){
-    show(`${url}/${props.src}`)
-  }
-})
+const url = import.meta.env.VITE_FILE_URL;
+const props = defineProps(['src', 'path', 'class', 'noImage', 'ratio', 'isThumbnail', 'alt', 'style', 'public']);
+const emits = defineEmits(['update:modelValue', 'click']);
+const image = ref(no_image);
 
-watch(()=>props.src,()=>{
-  if (props.src && props.src != ''){
-    show(`${url}/${props.src}`)
-  }
-})
-
-const show = async (url) =>{
-  const config = {
-    responseType: 'blob',
-    timeout: 40 * 1000,
-    headers: {
-      Accept: "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "authorization" : `Bearer ${localStorage.getItem('token')}`,
-    },
-  };
-
-  const toBase64 = file => new Promise((resolve, reject) => {
+const show = async (url) => {
+  try {
+    const response = await axios.get(url, {
+      responseType: 'blob',
+      timeout: 40000,
+      headers: {
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
     const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-  });
+    reader.onload = () => (image.value = reader.result);
+    reader.readAsDataURL(new File([response.data], 'str'));
+  } catch (error) {
+    console.error('Error fetching image:', error);
+  }
+};
 
-  await axios.get(url, config).then(async (response) => {
-    image.value = await toBase64(new File([response.data], 'str'))
-  });
-}
+const loadImage = () => {
+  if (props.src) {
+    show(`${url}/${props.src}`);
+  } else if (props.path) {
+    image.value = props.path;
+  }
+};
 
+onMounted(loadImage);
+watch(() => props.src, loadImage);
+watch(() => props.path, loadImage);
 </script>
+
 <template>
-  <img :src="image" :class="props.class" :style="props.style" :alt="props.alt" @click="emits('click')"/>
+  <img :src="image" :class="props.class" :style="[props.style, props.ratio ? { 'aspect-ratio': props.ratio } : '']" :alt="props.alt ?? 'دایه'" @click="emits('click')" />
 </template>
+
 <style scoped>
-img{
+img {
   width: 100%;
   object-fit: contain;
   aspect-ratio: 1;
